742bf59be2e8aae4b1ed6ed2b7d38c8dbc2a1e38
Rohit <rohit.w@indictranstech.com>
2014-05-29 01:41:23 +0530

    init

---
+++ library_management/config/desktop.py
@@ -0,0 +1,11 @@
+from frappe import _
+
+def get_data():
+	return {
+		"Library Management": {
+			"color": "#907474",
+			"icon": " icon-suitcase",
+			"type": "module",
+			"label": _("Library Management")
+		}
+	}
